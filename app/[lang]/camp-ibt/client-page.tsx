9c38bf0509b034922ab9aee65260f47fabd3a6a6
Galolara <glarabarbieri@gmail.com>
2026-08-24 17:27:23 -0400

    Update banner image in client-page.tsx

--- app/[lang]/camp-ibt/client-page.tsx
+++ app/[lang]/camp-ibt/client-page.tsx
@@ -153,7 +153,7 @@ export default function CampIBTClientPage({ lang, dict }: CampIBTClientPageProps
         <section className="bg-black pt-20">
           <div className="w-full">
             <Image
-              src="/images/banner-ibt.jpeg"
+              src="/images/IBT-2027.png"
               alt={t.hero.title}
               width={1920}
               height={640}
